c553a6c8b7dfc9e0591f3f708dc6e62768e462d6
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-05-15 15:45:26 -0700

    added some functions

--- functions.js
+++ functions.js
@@ -1,34 +1,56 @@
+// Add two numbers with javascript
 function add(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
    return input1 + input2;
 }
+// subtract two numbers with javascript
 function subtract(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
    return input1 - input2;
 }
+// multiply two numbers with javascript
 function multiply(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
    return input1 * input2;
 }
+// divide two numbers with javascript
 function divide(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
    return input1 / input2;
 }
-// increment by 1
+// increment a number with javascript
 function increment(input1) {
+   // increments input by 1
    input1++;
    return input1;
 }
-// decrement by 1
+// decrement a number with javascript
 function decrement(input1) {
+   // decrements input by 1
    input1--;
+   return input1;
+}
+// concatenating strings with plus operator
+function concatenate(input1, input2) {
+   //concatenates input1 and input2
+   input1 = String(input1);
+   input1 + input2;
+   // return input1 + input2;
+   return input1;
+}
+// concatenating strings with plus equals operator
+function concatenatingPlusEquals(input1, input2) {
+   // concatenates input1 and input2
+   input1 = String(input1);
+   input += input2;
+   // return input1+ input2;
    return input1;
 }
