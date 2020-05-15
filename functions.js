e9d08f6c67a7d00a9fd7dbcf2667658cd210f019
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-05-14 23:07:48 -0700

    start of my functions library

--- functions.js
+++ functions.js
@@ -4,3 +4,31 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+function subtract(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 - input2;
+}
+function multiply(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 * input2;
+}
+function divide(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 / input2;
+}
+// increment by 1
+function increment(input1) {
+   input1++;
+   return input1;
+}
+// decrement by 1
+function decrement(input1) {
+   input1--;
+   return input1;
+}
